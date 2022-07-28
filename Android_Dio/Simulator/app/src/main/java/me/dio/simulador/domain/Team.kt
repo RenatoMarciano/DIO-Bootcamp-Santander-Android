901203f7ae5b03e62e70f757ceaea69a6c2531bd
Renato Marciano <renato_vtr@hotmail.com>
2022-07-28 11:44:09 -0300

    Configurando Retrofit - Consumindo API

--- Android_Dio/Simulator/app/src/main/java/me/dio/simulador/domain/Team.kt
+++ Android_Dio/Simulator/app/src/main/java/me/dio/simulador/domain/Team.kt
@@ -1,7 +1,12 @@
 package me.dio.simulador.domain
 
+import com.google.gson.annotations.SerializedName
+
 data class Team (
+    @SerializedName("nome")
     val nome: String,
+    @SerializedName("forca")
     val stars: Int,
+    @SerializedName("imagem")
     val image: String
 )
