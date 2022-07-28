4df8aafd9fd79de89f00bade2ba2eb1a79846b34
Renato Marciano <renato_vtr@hotmail.com>
2022-07-27 23:09:54 -0300

    Criando class DetailActivity com binding

--- Android_Dio/Simulator/app/src/main/java/me/dio/simulador/MainActivity.java
+++ Android_Dio/Simulator/app/src/main/java/me/dio/simulador/MainActivity.java
@@ -7,7 +7,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import me.dio.simulador.databinding.ActivityMainBinding;
 
-public class MainActivityJava extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity {
 
     private ActivityMainBinding binding;
 
