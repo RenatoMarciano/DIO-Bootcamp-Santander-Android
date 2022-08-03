2a20da514bcdd926dfdd9e579823b2289846333a
Renato Marciano <renato_vtr@hotmail.com>
2022-08-03 11:20:17 -0300

    Listando Nossas Notícias Favoritas Via Room

--- Android_Dio_Java/app/src/main/java/me/dio/soccernews/ui/news/NewsFragment.java
+++ Android_Dio_Java/app/src/main/java/me/dio/soccernews/ui/news/NewsFragment.java
@@ -1,6 +1,5 @@
 package me.dio.soccernews.ui.news;
 
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -10,9 +9,8 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.room.Room;
 
-import me.dio.soccernews.data.remote.local.AppDatabase;
+import me.dio.soccernews.MainActivity;
 import me.dio.soccernews.databinding.FragmentNewsBinding;
 import me.dio.soccernews.ui.adapters.NewsAdapter;
 
@@ -20,23 +18,33 @@ import me.dio.soccernews.ui.adapters.NewsAdapter;
 public class NewsFragment extends Fragment {
 
     private FragmentNewsBinding binding;
-    private AppDatabase db;
 
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        NewsViewModel homeViewModel = new ViewModelProvider(this).get(NewsViewModel.class);
+        NewsViewModel newsViewModel = new ViewModelProvider(this).get(NewsViewModel.class);
 
         binding = FragmentNewsBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        db = Room.databaseBuilder(getContext(), AppDatabase.class, "soccer-news")
-                .allowMainThreadQueries()
-                .build();
-
         binding.rvNews.setLayoutManager(new LinearLayoutManager(getContext()));
-        homeViewModel.getNews().observe(getViewLifecycleOwner(), news -> {
+        newsViewModel.getNews().observe(getViewLifecycleOwner(), news -> {
             binding.rvNews.setAdapter(new NewsAdapter(news, updatedNews -> {
-                db.newsDao().insert(updatedNews);
+                MainActivity activity = (MainActivity) getActivity();
+                if (activity != null) {
+                    activity.getDb().newsDao().save(updatedNews);
+                }
             }));
+        });
+
+        newsViewModel.getState().observe(getViewLifecycleOwner(), state -> {
+            switch (state) {
+                case DOING:
+                    //TODO: Incluir SwipeRefresLayout (loading).
+                case DONE:
+                    //TODO: Finalizar SwipeRefresLayout (loading).
+                case ERROR:
+                    //TODO: finalizar SwipeRefresLayout (loading).
+                    //TODO: Mostrar um erro genérico.
+            }
         });
         return root;
     }
--- Android_Dio_Java/app/src/main/java/me/dio/soccernews/ui/news/NewsViewModel.java
+++ Android_Dio_Java/app/src/main/java/me/dio/soccernews/ui/news/NewsViewModel.java
@@ -21,7 +21,12 @@ import retrofit2.converter.gson.GsonConverterFactory;
 
 public class NewsViewModel extends ViewModel {
 
+    public enum State {
+        DOING, DONE, ERROR;
+    }
+
     private final MutableLiveData<List<News>> news = new MutableLiveData<>();
+    private final MutableLiveData<State> state = new MutableLiveData<>();
     private final SoccerNewsApi api;
 
     public NewsViewModel() {
@@ -35,13 +40,16 @@ public class NewsViewModel extends ViewModel {
     }
 
     private void findNews() {
+        state.setValue(State.DOING);
         api.getNews().enqueue(new Callback<List<News>>() {
             @Override
             public void onResponse(Call<List<News>> call, Response<List<News>> response) {
                 if (response.isSuccessful()) {
                     news.setValue(response.body());
+                    state.setValue(State.DONE);
                 } else {
                     // TODO Pensar em uma estratégia de tratamento de erros.
+                    state.setValue(State.ERROR);
                 }
             }
 
@@ -53,7 +61,7 @@ public class NewsViewModel extends ViewModel {
         });
     }
 
-    public LiveData<List<News>> getNews() {
-        return news;
-    }
+    public LiveData<List<News>> getNews() { return news; }
+
+    public LiveData<State> getState() { return state; }
 }
